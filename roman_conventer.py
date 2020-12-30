34320077a8f49fd3f172584e91daed5c18ed6317
Bartosz <bartosz.rc@gmail.com>
2020-12-30 15:56:50 +0100

    Complete kata

--- roman_conventer.py
+++ roman_conventer.py
@@ -1,5 +1,16 @@
 
 
 def to_roman_conv(arabic_number):
-    if arabic_number <= 3:
-        return "I" * arabic_number
+    roman_list = ["C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+    arab_list = [100, 90, 50, 40, 10, 9, 5, 4, 1]
+
+    convert = ""
+    n = 0
+    while arabic_number > 0:
+
+        for _ in range(arabic_number // arab_list[n]):
+            convert += roman_list[n]
+            arabic_number -= arab_list[n]
+        n += 1
+    return convert
+
--- roman_conventer_test.py
+++ roman_conventer_test.py
@@ -5,7 +5,24 @@ import pytest
 @pytest.mark.parametrize('arabic_number, expected_roman', (
             (1, "I"),
             (2, "II"),
-            (3, "III")
+            (3, "III"),
+            (4, "IV"),
+            (5, "V"),
+            (8, "VIII"),
+            (9, "IX"),
 ))
 def test_convert_digits_less_than_ten(arabic_number, expected_roman):
+    assert to_roman_conv(arabic_number) == expected_roman
+
+
+@pytest.mark.parametrize('arabic_number, expected_roman', (
+            (10, "X"),
+            (22, "XXII"),
+            (43, "XLIII"),
+            (54, "LIV"),
+            (85, "LXXXV"),
+            (98, "XCVIII"),
+            (99, "XCIX"),
+))
+def test_convert_digits_less_than_100(arabic_number, expected_roman):
     assert to_roman_conv(arabic_number) == expected_roman
